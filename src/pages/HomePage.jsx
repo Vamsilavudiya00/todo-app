f184b7e04199ebbaa9e673b61a73fb68fd36da24
Vamsilavudiya00 <vamsilavadiyaa@gmail.com>
2023-12-20 09:19:34 +0530

    delete button added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import TodoCard from "../components/TodoCard";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseconfig";
 
+
+
 function HomePage() {
   const [getData, setGetData] = useState([]);
 
@@ -10,7 +12,7 @@ function HomePage() {
     const getdataFunction = async () => {
       const querySnapshot = await getDocs(collection(db, "data"));
       querySnapshot.forEach((doc) => {
-        setGetData((prev) => [...prev, doc.data()]);
+        setGetData((prev) => [...prev, {id: doc.id,title:doc.data().title,description:doc.data().description,user:doc.data().user}]);
       });
       console.log(getData)
     };
@@ -19,9 +21,11 @@ function HomePage() {
 
   console.log(getData)
 
+  
+
   return (
     <div className="w-full px-[12%] flex flex-col items-center pt-16">
-      {getData.map((data, index) => <TodoCard key={index} title={data.title} desc={data.description} user={data.user} />)}
+      {getData.map((data, index) => <TodoCard key={index} title={data.title} id={data.id} desc={data.description} user={data.user} />)}
       <button className="bg-pink-600 text-white px-6 py-2 rounded-xl w-fit my-4">
         LoadMore
       </button>
