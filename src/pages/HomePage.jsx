66ad2262ef7b4b8428e5f4bbd77adc6208806525
Vamsilavudiya00 <vamsilavadiyaa@gmail.com>
2023-12-20 13:06:36 +0530

    load more

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import { db } from "../../firebaseconfig";
 
 function HomePage() {
   const [getData, setGetData] = useState([]);
+  const [clickTime, setClickTime] = useState(1);
 
   useEffect(() => {
     const getdataFunction = async () => {
@@ -14,19 +15,15 @@ function HomePage() {
       querySnapshot.forEach((doc) => {
         setGetData((prev) => [...prev, {id: doc.id,title:doc.data().title,description:doc.data().description,user:doc.data().user}]);
       });
-      console.log(getData)
     };
     getdataFunction()
   },[]);
 
-  console.log(getData)
-
-  
 
   return (
     <div className="w-full px-[12%] flex flex-col items-center pt-16">
-      {getData.map((data, index) => <TodoCard key={index} title={data.title} id={data.id} desc={data.description} user={data.user} />)}
-      {getData.length > 5? <><button className="bg-pink-600 text-white py-2 px-6 my-6 rounded-xl">Load More</button></>:<></>}
+      {(getData.slice(0,getData.length<(clickTime*5)?getData.length:clickTime*5)).map((data, index) => <TodoCard key={index} title={data.title} id={data.id} desc={data.description} user={data.user} />)}
+      {getData.length > 5? <><button onClick={() => setClickTime(clickTime + 1)} className="bg-pink-600 text-white py-2 px-6 my-6 rounded-xl">Load More</button></>:<></>}
     </div>
   );
 }
