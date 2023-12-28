2c18a213b5f8a8d01a3aa6225034d82f6aaab61c
Vamsilavudiya00 <vamsilavadiyaa@gmail.com>
2023-12-28 14:21:29 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import NavBar from './components/NavBar'
 import CreateTodoPage from './pages/CreateTodoPage'
 import EditPost from './pages/EditPost'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -15,7 +16,7 @@ function App() {
         <Route path='/create-post' element={<CreateTodoPage />}/>
         <Route path='/:uid' element={<EditPost />}/>
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='mt-5 border text-center py-2'>
+        <p className='text-sm'>&#169; All Rights Reserved By - <span className='font-semibold'>Vamsi Lavudiya</span></p>
+    </div>
+  )
+}
+
+export default Footer
