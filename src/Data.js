a7edae74db1a5545cf6f5050e1b23fba7ac9be55
lavudiya vamsi <vamsilavadiyaa@gmail.com>
2023-12-29 22:46:29 +0530

    revert data

---
+++ src/Data.js
@@ -0,0 +1,156 @@
+export const data = [
+    {
+        id:1,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:2,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:3,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+    {
+        id:4,
+        title: " create vite app",
+        description: "hello everyone this is the wonderful life as a developer",
+        user:"vamsi",
+        date: "12-2-1999"
+    },
+]
